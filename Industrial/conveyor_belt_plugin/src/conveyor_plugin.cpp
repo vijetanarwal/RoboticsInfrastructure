ff1de0d03b410a7ec92aaf854f5c75f4fc25c9d3
jepear19 <jesusper2010@gmail.com>
2026-07-12 18:36:41 +0200

    Change sausage spawn rate

--- Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
+++ Industrial/conveyor_belt_plugin/src/conveyor_plugin.cpp
@@ -77,7 +77,7 @@ public:
                     std::chrono::steady_clock::now() - startTime[_entity])
                 .count() / 1000.0;
 
-            if (elapsed < 18.5)
+            if (elapsed < 18.7)
             {
                 SetVelocity(_ecm, link, yaw);
             }
